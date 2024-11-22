303e4f8c8af872db9c26bc921279d7337102443d
Yan <yantmartins@live.com>
2024-11-22 13:59:34 -0400

    update bd

--- BD-usuario/usuario (1).sql
+++ BD-usuario/usuario (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 22-Nov-2024 às 18:53
+-- Tempo de geração: 22-Nov-2024 às 18:59
 -- Versão do servidor: 10.4.28-MariaDB
 -- versão do PHP: 8.2.4
 
@@ -42,7 +42,9 @@ CREATE TABLE `usuario` (
 INSERT INTO `usuario` (`id_usuario`, `nome`, `email`, `telefone`, `senha`) VALUES
 (12, 'Isabela Oliveira ', 'isaoliveira@gmail.com', '67999665542', '063a66de90b9b8f3c3409f91ce8ac86e'),
 (13, 'Yan Torres Martins', 'yantmartins@live.com', '67999698168', '25d55ad283aa400af464c76d713c07ad'),
-(15, 'Maria da Silva', 'maria123@gmail.com', '67999999988', 'e10adc3949ba59abbe56e057f20f883e');
+(15, 'Maria da Silva', 'maria123@gmail.com', '67999999988', 'e10adc3949ba59abbe56e057f20f883e'),
+(16, 'Lia Torres', 'liatorrespet@outlook.com', '67999887754', 'e35cf7b66449df565f93c607d5a81d09'),
+(17, 'Paulo Henrique', 'trupepaulo@gmail.com', '67993108222', 'dd41cb18c930753cbecf993f828603dc');
 
 --
 -- Índices para tabelas despejadas
@@ -62,7 +64,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
